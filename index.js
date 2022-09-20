024e9fc547a5e2d20fac347998a5cc3fc2c12ca7
MariuszLeonGebel <mariusz.gebel.programista@gmail.com>
2022-09-20 08:54:35 +0200

    Applicaton rebuild

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const fastify = require('fastify');
 const fs = require('fs').promises;
 const path = require('path');
 const app = fastify({logger: true});
+const currDate = "";
+
+app.register(require('@fastify/formbody'));
+
+app.register(require("@fastify/view"), {
+  engine: {
+    twig: require("twig"),
+  },
+});
+
 
 async function loadAllData() {
   const buffer = await fs.readFile("./public/data.json");
@@ -14,27 +24,71 @@ async function saveData(data) {
   await fs.writeFile("./public/data.json", s);
 }
 
-app.post("/data", async function(req, reply) {
-  const data = await loadAllData();
-  data.transactions.push(req.body);
-  await saveData(data);
-  reply.send("OK");
+// app.post("/data", async function(req, reply) {
+//   const data = await loadAllData();
+//   data.transactions.push(req.body);
+//   await saveData(data);
+//   reply.send("OK");
+// })
 
-  // const body = JSON.stringify(req.body)
-  // fs.writeFile("./public/data.json", body, function(err) {
-  //   if(err !== null) {
-  //     reply.statusCode = 500;
-  //     reply.send("Internal server error");
-  //     return;
-  //   }
-  //   reply.send("ok");
-  // })
-})
+const data = [
+  { id: 1, transactionType: "WY", transactionAmount: 1, transactionDate: "2022-09-04" },
+  { id: 2, transactionType: "WP", transactionAmount: 2, transactionDate: "2023-10-05" }
+]
+
+app.get('/', (req, reply) => {
+  reply.view('/views/index.twig', {
+      items: data
+  });
+});
+
+const getCurrentDate = () => {
+  let currentDate = new Date().toJSON().slice(0, 10);
+  console.log("transactionDate: " + currentDate); // "2022-06-17" 
+  return currentDate;
+}
+
+app.post('/data', function (req, reply) {
+  console.log(req.body);
+  const record = req.body;
+  record.id = data.length + 1;
+  record.transactionType = req.body.transactionType
+  record.transactionAmount = parseFloat(req.body.transactionAmount);
+  record.transactionDate = getCurrentDate();
+  
+  data.push(record);
+  saveData(data);
+  // loadAllData();
+  reply.redirect('/');
+  
+});
+
+
+// app.get('/data/:id', async function(req, reply) {
+//   const data = await loadAllData();
+//   const record = data.transactions.find(function(r) {
+//     return r.id === parseInt(req.params.id, 10);
+//   })
+//   reply.send(record);
+
+// });
+
+// app.put("/data/:id", async function(req, reply) {
+//   const data = await loadAllData();
+//   data.transactions.push(req.body);
+//   await saveData(data);
+//   reply.send("OK");
+// })
 
 app.register(require('@fastify/static'), {
   root: path.join(__dirname, 'public'),
-  prefix: '/', // optional: default '/'
+  prefix: '/public/', // optional: default '/'
 })
+
+// app.register(require('@fastify/static'), {
+//   root: path.join(__dirname, 'public'),
+//   prefix: '/public/', // optional: default '/'
+// })
 
 
 
--- public/script.js
+++ public/script.js
@@ -2,9 +2,11 @@ const amount = document.querySelector(".form-control");
 const balance = document.querySelector(".balance");
 const btnIn = document.querySelector(".btn-success");
 const btnOut = document.querySelector(".btn-danger");
+const btnPut = document.querySelector(".btn-info");
 const feedbackList = document.querySelector('.feedbacks');
 const badgeLetter = document.querySelector('feedback__letter')
 let badgeLetters = "";
+let maxId = 0;
 
 // GET
 const getTransactions = () => {
@@ -15,11 +17,54 @@ const getTransactions = () => {
 })
 }
 
-//OBIEKT: TRANSAKCJA
-const transactionItem = {
+// GET MAX ID
+const getMaxId = () => {
+  let maxIdArray = [];
+  fetch('./data.json').then(res => {
+  return res.json();
+}).then(data => {
+  console.log(data.transactions);
+  data.transactions.forEach(item => {
+    maxIdArray.push(item.id)
+  })
+  maxId = Math.max(...maxIdArray);
+  console.log(`MaxID: ${maxId}`);
+})
+}
+
+//OBIEKT: TRANSAKCJA POST
+const transactionItemPost = {
+  id: 3,
   transactionType: "WY",
   transactionAmount: 23300,
   transactionDate: "2022-09-04"
+}
+
+//OBIEKT: TRANSAKCJA PUT
+const transactionItemPut = {
+  id: 1,
+  transactionType: "WY",
+  transactionAmount: 10000,
+  transactionDate: "2019-08-01"
+}
+
+//PUT
+const putTransaction = () => {
+  fetch('/data', {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(transactionItemPut)
+  }).then(response => {
+    if(!response.ok) {
+      console.log('Something went wrong (PUT transaction)');
+      return;
+    } 
+      console.log("PUT successfully submitted")
+      getTransactions();
+  }).catch(error => console.log(error));
 }
 
 //POST
@@ -30,20 +75,20 @@ const postTransaction = () => {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(transactionItem)
+    body: JSON.stringify(transactionItemPost)
   }).then(response => {
     if(!response.ok) {
       console.log('Something went wrong');
       return;
     } 
-      console.log("Successfully submitted")
+      console.log("POST successfully submitted")
       getTransactions();
   }).catch(error => console.log(error));
 }
 
 //PRZYCISKI
 const testIn = () => {
-  getTransactions();
+  getMaxId();
 }
 btnIn.addEventListener("click", testIn)
 
@@ -51,6 +96,11 @@ const testOut = () => {
   postTransaction();
 }
 btnOut.addEventListener("click", testOut)
+
+const testPut = () => {
+  putTransaction();
+}
+btnOut.addEventListener("click", testPut)
 
 
 
